4a15c55d1d1d81c853a27c04435cacf92785a343
Jyothi7036 <pallajyothiprasad@gmail.com>
2025-07-29 11:42:05 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",          
-  host: "localhost",
-  database: "student_db",
-  password: "1998",
-  port: 5432,
+  connectionString:process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 module.exports = pool;
